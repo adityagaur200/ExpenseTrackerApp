22855fb2d1336d7bec6340b2b5a7d3f3ce8c52bb
adityagaur200 <115742009+adityagaur200@users.noreply.github.com>
2024-08-22 10:46:03 +0530

    Add functionality

--- TrackerBackend/src/main/java/com/tracker/trackerbackend/repo/MoneyInRepo.java
+++ TrackerBackend/src/main/java/com/tracker/trackerbackend/repo/MoneyInRepo.java
@@ -4,7 +4,6 @@ import com.tracker.trackerbackend.model.MoneyIn;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface MoneyInRepo extends MongoRepository<MoneyIn,Integer>
 {
 
--- TrackerBackend/src/main/java/com/tracker/trackerbackend/repo/MoneyOutRepo.java
+++ TrackerBackend/src/main/java/com/tracker/trackerbackend/repo/MoneyOutRepo.java
@@ -4,7 +4,7 @@ import com.tracker.trackerbackend.model.MoneyOut;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface MoneyOutRepo extends MongoRepository<MoneyOut, Integer>
 {
 }
